af2ac1cbbeee8df36938bc0226fb289066a42506
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-12-31 19:56:26 -0300

    add new lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,11 @@ lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
+
+  local opts = { buffer = bufnr, remap = false }
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 end)
 
 require('mason').setup({})
