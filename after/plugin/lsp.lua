e2fb4f12b7aa1b360b340bb9d631c13a064fc90a
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-01-02 01:41:22 -0300

    feat: add new lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,20 +1,45 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 
-  local opts = { buffer = bufnr, remap = false }
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<C-l>", function() vim.cmd('LspZeroFormat') end, opts)
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    lsp_zero.default_setup,
-  },
+    ensure_installed = {},
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
+
+require('null-ls').setup({
+    sources = {
+        require('null-ls').builtins.diagnostics.eslint_d.with({
+            condition = function(utils)
+                return utils.root_has_file({ '.eslintrc.js' })
+            end
+        }),
+        require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { "NvimTree" } }),
+        require('null-ls').builtins.formatting.eslint_d.with({
+            condition = function(utils)
+                return utils.root_has_file({ '.eslintrc.js' })
+            end
+        }),
+        require('null-ls').builtins.formatting.phpcsfixer,
+        require('null-ls').builtins.formatting.prettierd,
+    }
+})
+
+require('mason-null-ls').setup({ automatic_installation = true })
+
+vim.api.nvim_create_user_command('Format', vim.lsp.buf.format, {})
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,16 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+vim.keymap.set('n', '<leader>ps', function ()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+require('telescope').setup({
+	defaults = {
+		layout_config = {
+			vertical = { width = 0.5 }
+		},
+		file_ignore_patterns = { "node%_modules/.*", "vendor/.*" }
+	}
+})
